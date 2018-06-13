aad9acdb43609fde4768486462ddfd081bd790ee
hepicek <ondrej.hepnar@gmail.com>
2018-06-13 11:12:34 +0200

    migration user_list_entried added and relationships added

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -13,18 +13,11 @@ class ListController extends Controller
 {
     use AuthenticatesUsers;
     public function index(Request $request) {
-        dd(Auth::id());
-        // dd($request);
-        // dd(Sentry::getUser()->id);
         return Auth::id();
     }
     public function getUserLists(Request $request) {
         $current_user_id = Auth::id();
         $user_lists = User_List::where('owner_id', $current_user_id)->get()->getDictionary();
-
-        // foreach($user_lists as $list) {
-        //     $list['items'] = List_Item::where('list_id', $list['id'])->get();
-        // }
 
         foreach($user_lists as $list) {
             $list->items = [];
@@ -34,20 +27,14 @@ class ListController extends Controller
             ->leftJoin('imdb_movie', 'show_id', '=', 'imdb_movie.imdb_id')
             ->whereIn('list_id', array_keys($user_lists))
             ->get()->getDictionary();
-        // var_dump($list_items);
         foreach($list_items as $key => $list_item) {
             $array = $user_lists[$list_item->list_id]->items;
 
             $array[$key] = $list_item;
             $user_lists[$list_item->list_id]->items = $array;
-
-
-            
-            
         }
 
         return response()->json(['response' => $user_lists], 200);
-        // $user_lists, $list_items
     }
 
     public function storeUserList(Request $request)
