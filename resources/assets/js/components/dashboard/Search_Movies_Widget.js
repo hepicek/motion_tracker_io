7a588ceafc4c8a9bb6b00893838e73c1c69228f6
hepicek <ondrej.hepnar@gmail.com>
2018-06-19 14:42:40 +0200

    arrow for movie item

--- resources/assets/js/components/dashboard/Search_Movies_Widget.js
+++ resources/assets/js/components/dashboard/Search_Movies_Widget.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import SEARCH_RESULTS_ITEM_WIDGET from './search_widget_components/Search_Results_Item_Widget';
 
 
 const SEARCH_MOVIES_WIDGET = (props) => {
-        let searchResults = props.searchResults.map((movie, id) => (
-            <div
+    let searchResults = props.searchResults.map((movie, id) => (
+        <div
             key={'search-' + id}
-            >
-                <SEARCH_RESULTS_ITEM_WIDGET searchResultsItem={movie}/>
-            </div>
+        >
+            <SEARCH_RESULTS_ITEM_WIDGET searchResultsItem={movie}/>
+        </div>
 
-            ));
-        return (
-            <div>
-                {searchResults}
-            </div>
-        )
-    };
-
-
-
+    ));
+    return (
+        <div>
+            {searchResults}
+        </div>
+    )
+};
 
 
 export default SEARCH_MOVIES_WIDGET;
--- resources/assets/js/components/dashboard/search_widget_components/Search_Results_Item_Widget.js
+++ resources/assets/js/components/dashboard/search_widget_components/Search_Results_Item_Widget.js
@@ -4,6 +4,7 @@ const SEARCH_RESULTS_ITEM_WIDGET = (props) => {
     let movieItem = props.searchResultsItem;
     return (
         <div className='searchResultsItem'>
+            <i className="fa fa-caret-down"/>
             <p>{movieItem.name}</p>
             <p>{movieItem.year}</p>
             <p>{movieItem.rating}</p>
