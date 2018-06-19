0573626e88bfefec2e5e531be1d08926726a6906
hepicek <ondrej.hepnar@gmail.com>
2018-06-19 11:26:02 +0200

    search movies widget in progress

---
+++ resources/assets/js/components/dashboard/Search_Movies_Widget.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import SEARCH_MOVIES_WIDGET from "./Top_Movies_Widget";
+
+
+const SEARCH_MOVIES_WIDGET = (props) => {
+        let searchResults = props.searchResults.map(movie => (
+            <div>
+                
+            </div>
+
+            ));
+    };
+
+
+
+
+
+export default SEARCH_MOVIES_WIDGET;
