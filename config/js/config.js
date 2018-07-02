f331d71df4828fc4f317bb0a23bc3701ac21d976
Timothy A Breeding <tim.breeding@yahoo.com>
2018-07-02 11:01:23 +0200

    Added some JS config variable (AWS_URL)

--- config/js/config.js
+++ config/js/config.js
@@ -1,5 +1,7 @@
 const TMDB_KEY = '2737df4078e7e961dbae1994dd5ff332';
+const AWS_URL = 'https://s3.eu-central-1.amazonaws.com/motion-tracker-data/';
 
 module.exports = {
-    TMDB_KEY
+    TMDB_KEY,
+    AWS_URL
 }
